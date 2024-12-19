225ce4f6a8f60faf6075027f47133b12738bdc34
ratipriya212003 <133866565+ratipriya212003@users.noreply.github.com>
2024-12-19 23:32:09 +0530

    Add dotenv for environment variables

--- hotelback/db/config.js
+++ hotelback/db/config.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
-const DB =
-"mongodb+srv://alphatechpune24:AlphaStay2024@cluster0.illtx.mongodb.net/AlphaStay?retryWrites=true&w=majority";
+require('dotenv').config();
+const DB = process.env.MONGODB_URI;
 
   const connectDB = async () => {
   await mongoose
-    .connect(DB)
+    .connect(DB ,  { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
       console.log("connected to mongoDB");
     })
     .catch((error) => {
       console.log("error in connecting to mongoDB", error);
+      process.exit(1);
     });
 };
 
